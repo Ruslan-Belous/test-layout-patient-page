02043d221cd779253bd163598b03ad9e82a303a5
Ruslan.Belous <r.bilous@viseven.com>
2022-08-03 13:26:33 +0300

    deploy

---
+++ precache-manifest.ff33d656ac3087efd66f039e9ff27af6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be972b084c7c148498c7",
+    "url": "/test-layout-patient-page/css/app.4608a44d.css"
+  },
+  {
+    "revision": "a2fe5f60837618c5d7cd",
+    "url": "/test-layout-patient-page/css/chunk-vendors.34378d7f.css"
+  },
+  {
+    "revision": "39bfea5e86f5f41c9d2896dbbed6791b",
+    "url": "/test-layout-patient-page/fonts/MaterialIcons-Regular.39bfea5e.woff"
+  },
+  {
+    "revision": "ae8b1248595e70a828b880b9c56963da",
+    "url": "/test-layout-patient-page/fonts/MaterialIcons-Regular.ae8b1248.eot"
+  },
+  {
+    "revision": "b073f5972d9c4cc1b8ae8e071e441376",
+    "url": "/test-layout-patient-page/fonts/MaterialIcons-Regular.b073f597.woff2"
+  },
+  {
+    "revision": "b9968c1adf542baec8d2b298d3ff437c",
+    "url": "/test-layout-patient-page/fonts/MaterialIcons-Regular.b9968c1a.ttf"
+  },
+  {
+    "revision": "92c5cc11e9efe7f02d1446b4bc6b82f6",
+    "url": "/test-layout-patient-page/img/docPhoto.92c5cc11.jpeg"
+  },
+  {
+    "revision": "1d6337fee94a791c1ff19aa0c47c8ffd",
+    "url": "/test-layout-patient-page/index.html"
+  },
+  {
+    "revision": "be972b084c7c148498c7",
+    "url": "/test-layout-patient-page/js/app.e50739e6.js"
+  },
+  {
+    "revision": "a2fe5f60837618c5d7cd",
+    "url": "/test-layout-patient-page/js/chunk-vendors.152eeead.js"
+  },
+  {
+    "revision": "d1cfa7194c48caf0bfcdd213c9d85e98",
+    "url": "/test-layout-patient-page/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-layout-patient-page/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/test-layout-patient-page/precache-manifest.ff33d656ac3087efd66f039e9ff27af6.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "test-layout-patient-page"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
